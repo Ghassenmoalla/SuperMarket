e5fe7c8a92f84ca403d880d4270d8f7eb938f5cc
MOALLA Ghassen <gmoalla@mantu.com>
2022-12-29 10:57:09 +0100

    first commit

---
+++ Supermarket/Program.cs
@@ -0,0 +1,30 @@
+using Supermarket;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+
+builder.Services.AddHealthChecks()
+  .AddCheck<SampleHealthCheack>("Sample"); var app = builder.Build();
+
+app.MapHealthChecks("/health");
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
---
+++ Supermarket/SampleHealthCheack.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Supermarket
+{
+    public class SampleHealthCheack : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(
+        HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            var isHealthy = false;
+
+            // ...
+
+            if (isHealthy)
+            {
+                return Task.FromResult(
+                    HealthCheckResult.Healthy("A healthy result."));
+            }
+
+            return Task.FromResult(
+                new HealthCheckResult(
+                    context.Registration.FailureStatus, "An unhealthy result."));
+        }
+
+
+    }
+}
